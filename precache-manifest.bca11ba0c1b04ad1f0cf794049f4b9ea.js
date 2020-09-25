452544181da4ace75f68867a88ad263281100d89
ojas032 <corpojasltd@gmail.com>
2020-09-25 16:06:28 +0530

    Updates

---
+++ precache-manifest.bca11ba0c1b04ad1f0cf794049f4b9ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007d8ef2fb0deb987054ee8aacf5e27e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "11bb7cf6dcddb4b22a1e",
+    "url": "/Portfolio/static/css/main.000cdf35.chunk.css"
+  },
+  {
+    "revision": "7bd2f405d6d74a2a5e9a",
+    "url": "/Portfolio/static/js/2.7eb6c2cc.chunk.js"
+  },
+  {
+    "revision": "11bb7cf6dcddb4b22a1e",
+    "url": "/Portfolio/static/js/main.197067b8.chunk.js"
+  },
+  {
+    "revision": "a218fe782f6b1aff2f5c",
+    "url": "/Portfolio/static/js/runtime-main.95dee7a4.js"
+  },
+  {
+    "revision": "651635232059af073d1386ecf841d015",
+    "url": "/Portfolio/static/media/img.65163523.jpg"
+  }
+]);
